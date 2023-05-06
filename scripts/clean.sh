c60c3b5c4de33fbf60b0c3631637cdb7c6b82514
Franco Basaglia <francobasaglia94@gmail.com>
2023-05-06 20:31:27 -0300

    Scripts added.

---
+++ scripts/clean.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Clean dependencies
+rm -rf ./node_modules
+
+# Clean output directory
+rm -rf ./lib
+
+# Clean test coverage output directory
+rm -rf ./coverage
---
+++ scripts/prepare-publish.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Install dependencies
+yarn install
+
+# Run linter
+yarn lint
+
+# Run tests
+yarn test
+
+# Build project
+yarn build
